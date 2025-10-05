c03c04bfda490155374f7edb807956878fd1f40a
Adrien JALLAIS <adrien.jallais@protonmail.com>
2025-10-05 14:36:01 +0200

    allow timeUp query param

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center h-screen text-center">
-    <TooLate v-if="eventTime.isTooLate" />
-    <TooEarly v-else-if="eventTime.isTooEarly" />
+    <TooLate v-if="eventTime.isTooLate && !isTimeUp" />
+    <TooEarly v-else-if="eventTime.isTooEarly && !isTimeUp" />
     <template v-else>
       <h1 class="text-3xl font-bold mb-6">{{ $t('gameName') }}</h1>
       <div class="flex flex-wrap gap-2 mb-4">
@@ -25,6 +25,7 @@
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import { useDeckStore, useEventTime } from '@/stores'
 import { TooLate, TooEarly } from '@/components'
 import type { Deck } from '@/models'
@@ -32,8 +33,11 @@ import type { Deck } from '@/models'
 const decks = ref<Map<string, Deck>>(new Map())
 const deckStore = useDeckStore()
 const eventTime = useEventTime()
+const route = useRoute()
+const isTimeUp = ref(false)
 
 onMounted(async () => {
+  isTimeUp.value = route.query.timeUp === 'true'
   if (deckStore.isEmpty()) await deckStore.loadDecks()
   decks.value = deckStore.getDecks()
 })
